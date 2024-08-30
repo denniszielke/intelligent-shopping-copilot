44b91498e5227c8ada2c89f5437f7e9fbfd16c47
denniszielke <11569044+denniszielke@users.noreply.github.com>
2024-08-30 09:24:21 +0200

    feat: added langgraph

--- src/tools/app.py
+++ src/tools/app.py
@@ -1,22 +1,20 @@
 import os
 import dotenv
 import random
-from typing import Annotated, TypedDict, Literal, Any, List, Dict
-from pydantic import BaseModel, Field
-from typing import  Literal
+from typing import Annotated, TypedDict, Literal, Any, List
 from typing_extensions import TypedDict
 
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from azure.core.credentials import AzureKeyCredential
 
 import streamlit as st
-
-import tiktoken
-from langchain_core.callbacks import BaseCallbackHandler
+from langchain_community.callbacks.streamlit import (
+    StreamlitCallbackHandler,
+)
 from openai import AzureOpenAI
 from langchain_openai import AzureChatOpenAI
 from langchain_openai import AzureOpenAIEmbeddings
-from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, BaseMessage
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 from langchain_core.runnables import RunnableLambda, RunnableWithFallbacks
 from langchain_core.tools import tool
 from langchain_core.prompts import ChatPromptTemplate
@@ -25,12 +23,10 @@ from langchain_core.prompts import PromptTemplate
 from langchain.agents import AgentExecutor, create_structured_chat_agent, create_react_agent
 from langchain import agents
 
-from langgraph.graph import MessagesState
 from langgraph.graph.message import add_messages
 from langgraph.graph import END, StateGraph, START
-from langgraph.prebuilt import ToolNode
 from langgraph.graph.message import AnyMessage, add_messages
-
+from langgraph.prebuilt import ToolNode
 
 from azure.search.documents import SearchClient
 from azure.search.documents.models import (
@@ -47,33 +43,6 @@ st.set_page_config(
 
 st.title("💬 AI shopping copilot")
 st.caption("🚀 A Bot that can recommend the right products for you")
-
-def num_tokens_from_messages(messages: List[str]) -> int:
-    '''
-    Calculate the number of tokens in a list of messages. This is a somewhat naive implementation that simply concatenates 
-    the messages and counts the tokens in the resulting string. A more accurate implementation would take into account the 
-    fact that the messages are separate and should be counted as separate sequences.
-    If available, the token count should be taken directly from the model response.
-    '''
-    encoding = tiktoken.encoding_for_model("gpt-4o")
-    num_tokens = 0
-    content = ' '.join(messages)
-    num_tokens += len(encoding.encode(content))
-
-    return num_tokens
-
-class TokenCounterCallback(BaseCallbackHandler):
-    prompt_tokens: int = 0
-    completion_tokens: int = 0
-
-    def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
-        self.completion_tokens += 1
-
-    def on_chat_model_start(self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], **kwargs: Any) -> Any:
-        self.prompt_tokens += num_tokens_from_messages( [message.content for message in messages[0]])
-         
-
-callback = TokenCounterCallback()
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
@@ -103,8 +72,7 @@ if "AZURE_OPENAI_API_KEY" in os.environ:
         azure_deployment=os.getenv("AZURE_OPENAI_COMPLETION_DEPLOYMENT_NAME"),
         openai_api_version=os.getenv("AZURE_OPENAI_VERSION"),
         temperature=0,
-        streaming=True,
-        callbacks=[callback]
+        streaming=True
     )
     embeddings_model = AzureOpenAIEmbeddings(    
         azure_deployment = os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME"),
@@ -126,8 +94,7 @@ else:
         openai_api_version=os.getenv("AZURE_OPENAI_VERSION"),
         temperature=0,
         openai_api_type="azure_ad",
-        streaming=True,
-        callbacks=[callback]
+        streaming=True
     )
     embeddings_model = AzureOpenAIEmbeddings(    
         azure_deployment = os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME"),
@@ -158,18 +125,6 @@ if "session_id" not in st.session_state:
     print("started new session: " + st.session_state["session_id"])
     st.write("You are running in session: " + st.session_state["session_id"])
 
-
-class RecommendationResponse(BaseModel):
-    """Response from the recommendation API"""
-    name: str = Field(description="The name of the product")
-    category: str = Field(description="The category of the product")
-    description: str = Field(description="The description of the product")
-    personalization: str = Field(description="The personalization of the product recommendation")
-    price: float = Field(description="Your price of the product")
-    question: str = Field(description="The question asked to the user for additional recommendations")
-
-class AgentState(MessagesState):
-    final_response: RecommendationResponse
 
 def format_docs(docs):
     return "\n\n".join([d.page_content for d in docs])
@@ -227,64 +182,69 @@ def get_jwt() -> int:
 
 tools = [get_jwt, get_user_id, get_user_info, search_for_product, get_last_purchases]
 
-model_with_tools = llm.bind_tools(tools)
-model_with_structured_output = llm.with_structured_output(RecommendationResponse)
 
-# Force the model to use tools by passing tool_choice="any"    
-model_with_response_tool = llm.bind_tools(tools,tool_choice="auto")
+prompt_template_v1 = """\
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
 
+You are talking to customers. Users and customers are the same thing for you.
 
-# Define the function that calls the model
-def call_model(state: AgentState):
-    response = model_with_tools.invoke(state['messages'])
-    # We return a list, because this will get added to the existing list
-    return {"messages": [response]}
+Assistant is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
 
-# Define the function that responds to the user
-def respond(state: AgentState):
-    # We call the model with structured output in order to return the same format to the user every time
-    # state['messages'][-2] is the last ToolMessage in the convo, which we convert to a HumanMessage for the model to use
-    # We could also pass the entire chat history, but this saves tokens since all we care to structure is the output of the tool
-    response = model_with_structured_output.invoke([HumanMessage(content=state['messages'][-2].content)])
-    # We return the final answer
-    return {"final_response": response}
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. 
 
-# Define the function that determines whether to continue or not
-def should_continue(state: AgentState):
-    messages = state["messages"]
-    last_message = messages[-1]
-    # If there is no function call, then we respond to the user
-    if not last_message.tool_calls:
-        return "respond"
-    # Otherwise if there is, we continue
-    else:
-        return "continue"
+TOOLS:
 
-# Define a new graph
-workflow = StateGraph(AgentState)
+------
 
-# Define the two nodes we will cycle between
-workflow.add_node("agent", call_model)
-workflow.add_node("respond", respond)
-workflow.add_node("tools", ToolNode(tools))
+Assistant has access to the following tools:
 
-# Set the entrypoint as `agent`
-# This means that this node is the first one called
-workflow.set_entry_point("agent")
+{tools}
 
-# We now add a conditional edge
-workflow.add_conditional_edges(
-    "agent",
-    should_continue,
-    {
-        "continue": "tools",
-        "respond": "respond",
-    },
-)
+To use a tool, please use the following format:
 
-workflow.add_edge("tools", "agent")
-workflow.add_edge("respond", END)
-graph = workflow.compile()
+```
+
+Thought: Do I need to use a tool? Yes
+
+Action: the action to take, should be one of [{tool_names}]
+
+Action Input: the input to the action
+
+Observation: the result of the action
+
+```
+
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+
+Thought: Do I need to use a tool? No
+
+Final Answer: [your response here]
+
+```
+
+Begin!
+
+Previous conversation history:
+
+{chat_history}
+
+New input: {input}
+
+{agent_scratchpad}
+"""
+
+prompt = PromptTemplate.from_template(prompt_template_v1)
+agent = create_react_agent(llm, tools, prompt)
+
+agent_executor = agents.AgentExecutor(
+        name="Tools Agent",
+        agent=agent, tools=tools, verbose=True, handle_parsing_errors=True, max_iterations=10, return_intermediate_steps=True, 
+        # handle errors
+        error_message="I'm sorry, I couldn't understand that. Please try again.",
+    )
+ 
 
 human_query = st.chat_input()
 
@@ -292,34 +252,10 @@ if human_query is not None and human_query != "":
 
     st.session_state.chat_history.append(HumanMessage(human_query))
 
-    inputs = {
-        "messages": [
-            ("user", human_query),
-        ]
-    }
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent_executor.invoke(
+            {"input": human_query, "chat_history": st.session_state.chat_history}, {"callbacks": [st_callback]}, 
+        )
 
-    for event in graph.stream(inputs):  
-        for value in event.values():
-            print(value)
-
-            if ( "messages" in value):
-                # check if there is a message in the response
-                message = value["messages"][-1]
-                if ( isinstance(message, AIMessage) ):
-                    print("AI:", message.content)
-                    with st.chat_message("Agent"):
-                        if (message.content == ''):
-                            toolusage = ''
-                            for tool in message.tool_calls:
-                                print(tool)
-                                toolusage += "name: " + tool["name"] + "  \n\n"
-                            st.write("Using the following tools: \n", toolusage)
-                        else:
-                            st.write(message.content)
-                
-                if ( isinstance(message, ToolMessage) ):
-                    print("Tool:", message.content)
-                    with st.chat_message("Tool"):
-                        st.write(message.content.replace('\n\n', ''))
-
-        st.write("The total number of tokens used in this conversation was: ", callback.completion_tokens + callback.prompt_tokens)
+        ai_response = st.write(response["output"])
